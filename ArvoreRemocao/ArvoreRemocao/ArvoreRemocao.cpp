cc27bf05985f1f86b06739303632d25025817b8d
Pedro Tenerelli <106717267+zPedroDev@users.noreply.github.com>
2022-11-22 10:39:06 -0300

    Update ArvoreRemocao.cpp

--- ArvoreRemocao/ArvoreRemocao/ArvoreRemocao.cpp
+++ ArvoreRemocao/ArvoreRemocao/ArvoreRemocao.cpp
@@ -83,7 +83,7 @@ void menu()
 void inicializar()
 {
 
-	// provisÛrio porque n„o libera a memoria usada pela arvore
+	// provis√≥rio porque n√£o libera a memoria usada pela arvore
 	raiz = NULL;
 
 	cout << "Arvore inicializada \n";
@@ -244,9 +244,30 @@ void removerElementoArvore(NO* no, int valor) {
 
 
 	// caso 1: sem filhos	
-	
+	if(atual->dir == NULL && atual->esq == NULL) {
+		if (pai->dir == atual) {
+			pai->dir = NULL;
+		}
+		else {
+			pai->esq = NULL;
+		}
+		free(atual);
+		cout << "Elemento foi excluido com sucesso" << endl;
+	}
 
 	// caso 2: um filho	
+	if (atual->dir != NULL && atual->esq == NULL || atual->dir == NULL && atual->esq != NULL) {
+		if (atual->dir != NULL) {
+			NO* sucessor = atual->dir;
+			pai->dir = sucessor;
+		}
+		else {
+			NO* sucessor = atual->esq;
+			pai->dir = sucessor;
+		}
+		free(atual);
+		cout << "Elemento removido com sucesso!\n";
+	}
 	
 
 	// caso 3: dois filhos
@@ -277,7 +298,6 @@ void removerElementoArvore(NO* no, int valor) {
 
 
 }
-
 
 
 
